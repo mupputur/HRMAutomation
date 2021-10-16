f1826b226a02fdebd6355784b23fa0bf046d241b
manoahrkunchala <manohar.kunchala1@gmail.com>
2021-10-16 20:47:04 +0530

    Updated admin_page.py file

--- source/pages/admin_page.py
+++ source/pages/admin_page.py
@@ -54,3 +54,48 @@ config()
 # sel= Select(driver.find_element_by_id("emailConfigurationForm[cmbMailSendingMethod]"))
 # sel.select_by_index(0)
 # elem=driver.find_element_by_id("emailConfigurationForm_chkSendTestEmail").click()
+
+def navigate_admin():
+    admin_button=resp.find_element_by_id("menu_admin_viewAdminModule").click()
+#   resp.find_elements_by_id("menu_admin_UserManagement").click()
+#   resp.find_elements_by_id("menu_admin_viewSystemUsers").click()
+def user_details():
+    navigate_admin()
+
+    resp.find_element_by_name("searchSystemUser[userName]").send_keys("rajesh")
+    user_roll=Select(resp.find_element_by_xpath("//*[@id='searchSystemUser_userType']"))
+    user_roll.select_by_visible_text("Admin")
+    resp.find_element_by_name("searchSystemUser[employeeName][empName]").send_keys("rajesh kp ")
+    Select(resp.find_element_by_xpath("//*[@id='searchSystemUser_status']")).select_by_visible_text("Enabled")
+    search_result=resp.find_element_by_name("_search").click()
+
+#   return search_result
+"""
+recorrds_found=user_details()
+if recorrds_found!=0:
+    print("employee deatails matched")
+else:
+    add_employee_details()
+"""
+
+def add_employee_details():
+    resp.find_element_by_name("btnAdd").click()
+    resp.find_element_by_name("systemUser[employeeName][empName]").send_keys("Rohit@123")
+    resp.find_element_by_name("systemUser[userName]").send_keys("Rohit@1234567")
+    resp.find_element_by_name("systemUser[password]").send_keys("Rohit!@@#$$$$")
+    resp.find_element_by_name("systemUser[confirmPassword]").send_keys("Rohit!@@#$$$$")
+    resp.find_element_by_id('btnSave').click()
+    time.sleep(5)
+"""
+def details_delete():
+    resp.find_element_by_name("_reset").click()
+    resp.find_element_by_name("searchSystemUser[userName]").send_keys("Rohit@12345")
+    resp.find_element_by_name("_search").click()
+    resp.find_element_by_xpath("//*[@id='ohrmList_chkSelectAll']").click()
+    resp.find_element_by_name("btnDelete").click()
+    resp.find_elements_by_id("dialogDeleteBtn")[0].click()
+"""
+user_details()
+add_employee_details()
+#details_delete()
+
