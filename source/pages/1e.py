2bc3353bcf702beeb1bd02dc06bbcb27787df9c9
anitha1996 <anithabochhu123@gmail.com>
2021-10-17 16:05:34 +0530

    added 1e

---
+++ source/pages/1e.py
@@ -0,0 +1,9 @@
+#WAP to accept two integers and check whether they are equal or not
+x1=int(input("ENTER FIRST NUM: "))
+x2=int(input("ENTER SECOND NUM: "))
+if x1 == x2:
+    print("THE GIVEN TWO NUMBERS ARE EQUAL")
+else:
+    print("THE GIVEN TWO NUMBERS ARE NOT EQUAL")
+    
+
